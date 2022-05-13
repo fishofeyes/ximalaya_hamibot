961c06e2ff1127c893cc838ff93f0090516145f5
zoulin <zoulin@supermonkey.com.cn>
2022-05-13 11:50:05 +0800

    feat: submit

--- mao_tai.js
+++ mao_tai.js
@@ -1,5 +1,5 @@
 // launchApp("京东")
-
+// v1
 console.show()
 //注册触摸监听器
 id("com.jd.lib.productdetail.feature:id/add_2_car").findOne().click()
@@ -13,6 +13,7 @@ while(true) {
   console.log("提交订单 = %s", res)
 }
 
+// v2
 console.show()
 var res = true;
 while(true){
@@ -28,5 +29,26 @@ while(true){
 while(true) {
 	text("提交订单").findOne().click()
   console.log("提交订单 = %s", res)
+}
+
+// v3
+
+console.show()
+var res = true;
+while (res) {
+    if (!text('立即预约').exists()) {
+        console.log("---开始抢购")
+        id("com.jd.lib.productdetail.feature:id/add_2_car").findOne().click()
+        while (res) {
+            res = text("确定").findOne().click()
+        }
+    } else {
+        console.log("--等待抢购")
+    }
+
+}
+while (true) {
+    text("提交订单").findOne().click()
+    console.log("提交订单 = %s", res)
 }
 
